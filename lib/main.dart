3ffe4dbe1e861d8dc55dacfa442cef3fe5ddcd4b
artur gavrilenko <arturgavrilenko@appvesto.com>
2023-06-05 19:43:49 +0300

    removed unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:cat_facts/application.dart';
 import 'package:cat_facts/config/interceptors.dart';
 import 'package:cat_facts/config/logger.dart';
-import 'package:cat_facts/services/storage/storage.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   Logger.init(LocalLogger());
   WidgetsFlutterBinding.ensureInitialized();
   initInterceptors();
-  await Storage.instance().init();
   runApp(const Application());
 }
